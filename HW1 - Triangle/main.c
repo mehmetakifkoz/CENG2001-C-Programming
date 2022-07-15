4dbef06871cbe8af0ecca7d340d88e2df011e22c
Mehmet Akif KÖZ <53031435+mehmetakifkoz@users.noreply.github.com>
2022-07-15 10:27:53 +0300

    Create main.c

---
+++ HW1 - Triangle/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("*Triangle by Mehmet Akif KOZ 192010010023\n");
+    printf("*This program draws a triangle and displays it.\n");
+    printf("*Instructions: The program draws a triangle after you run it.\n\n");
+
+    printf("       /\\      \n");
+    printf("      /  \\     \n");
+    printf("     /    \\    \n");
+    printf("    /      \\   \n");
+    printf("    --------    \n");
+}
